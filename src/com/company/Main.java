5f5574235f4f41cf98310c0338be2dff71829bc3
razzmed <mederrazzakov@gmail.com>
2021-08-19 20:05:42 +0600

    methods part 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,30 @@ package com.company;
 
 public class Main {
 
+    public static int k = 20;
+
     public static void main(String[] args) {
 
-        int chargeLevel = 5;
+        System.out.println(k);
+
+        printText();
+//        addition(12, 16);
+//
+//        int num = multiply(50, 77, 100, 32);
+//        System.out.println(num);
+
+//        int chargeLevel = 5;
+//        int g = 10;
+//
+//        g = g + 5;
+//        System.out.println(g);
+
 
 //        if (chargeLevel <= 20) {
+//
+//            int j = 15;
+//            System.out.println(j);
+//            System.out.println(g);
 //            System.out.println("Зарядите устройство!");
 //        } else if (chargeLevel == 100) {
 //            System.out.println("Зарядка не требуется!");
@@ -23,21 +42,43 @@ public class Main {
 //            System.out.println("Можно гулять");
 //        }
 
-        int number = 4;
+//        int number = 4;
+//
+//        switch (number){
+//            case 1:
+//                System.out.println("Это число 1");
+//                break;
+//            case 2:
+//                System.out.println("Это число 2");
+//                break;
+//            case 3:
+//                System.out.println("Это число 3");
+//                break;
+//            default:
+//                System.out.println("Нет такого числа!!!");
+//        }
 
-        switch (number){
-            case 1:
-                System.out.println("Это число 1");
-                break;
-            case 2:
-                System.out.println("Это число 2");
-                break;
-            case 3:
-                System.out.println("Это число 3");
-                break;
-            default:
-                System.out.println("Нет такого числа!!!");
-        }
 
     }
+
+    public static void printText(){
+
+        System.out.println(k);
+
+//        System.out.println("Max");
+//        System.out.println("John");
+//        System.out.println("Cris");
+//        System.out.println("Anna");
+    }
+
+//    public static void addition(int a, int b){
+//        System.out.println(a + b);
+//
+//    }
+//
+//    public static int multiply(int num1, int num2, int num3, int num4){
+//        int result = (num1 * num2) + (num3 * num4);
+//        return result;
+//    }
+
 }
